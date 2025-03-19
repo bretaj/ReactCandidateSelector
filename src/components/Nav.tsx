b3b2e3dc3d997bcf03e80a8daf156cce996da395
bret jensen <jensenbret13@gmail.com>
2025-03-18 20:18:25 -0700

    got links to work

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,36 +1,38 @@
-const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
-  return (
-    <nav>
-    <p>Home</p>
-    <p>Potential Candidates</p>
-    </nav>
-  )
-};
 
-export default Nav;
 
-// TODO: can I use the routing instead?
-
-// import React from 'react';
-// import { Link, useLocation } from 'react-router-dom';
-
-// const Navigation: React.FC = () => {
-//   const location = useLocation();
-  
+// const Nav = () => {
+//   // TODO: Add necessary code to display the navigation bar and link between the pages
 //   return (
-//     <nav className="text-right">
-//       <ul>
-//         <li>
-              // css to get active links to show as darker color
-//           <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link>
-//         </li>
-//         <li>
-//           <Link to="/SavedCandidates" className={location.pathname === '/SavedCandidates' ? 'active' : ''}>Potential Candidates</Link>
-//         </li>
-//       </ul>
+//     <nav>
+//     <p>Home</p>
+//     <p>Potential Candidates</p>
 //     </nav>
-//   );
-// }
+//   )
+// };
 
-// export default Nav
+// export default Nav;
+
+// TODO: can I use this routing instead?
+
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+const Nav: React.FC = () => {
+  const location = useLocation();
+  
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+              {/* css to get active links to show as darker color */}
+          <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link>
+        </li>
+        <li>
+          <Link to="/SavedCandidates" className={location.pathname === '/SavedCandidates' ? 'active' : ''}>Potential Candidates</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav
