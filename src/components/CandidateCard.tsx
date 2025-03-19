39c3a65394d783339fe089254f97b8e30d85da35
bret jensen <jensenbret13@gmail.com>
2025-03-18 18:50:28 -0700

    some updating of card

---
+++ src/components/CandidateCard.tsx
@@ -0,0 +1,48 @@
+import type React from 'react';
+// TODO: Uncomment when Candidate interface is added
+// import type Candidate from '../interfaces/Candidate.interface';
+import { IoEyeOutline } from 'react-icons/io5';
+import { ImCross } from 'react-icons/im';
+import { CgPlayListAdd } from 'react-icons/cg';
+// TODO: This is the FilmCard code from the Mod13 mini project. Using this to see how much can just be 
+// copied over
+
+// TODO: Define FilmCardProps
+type CandidateCardProps = {
+  
+};
+
+const CandidateCard = () => {
+  return (
+    <>
+      {currentFilm?.Title ? (
+        <section className='CandidateCard'>
+          <figure>{/* TODO: Add film poster*/}</figure>
+          <article className='details'>
+            {/* TODO: Add film title, director, actors, released, and genre */}
+          </article>
+          <article className='plot'>{/* TODO: Add film plot here */}</article>
+          {/* If film is on Watch List or Seen It list film can be removed
+              Else film can be added to Watch List or Seen It list */}
+          {onWatchList || onSeenItList ? (
+            <aside className='icons'>
+              {/* TODO: Add remove film from localStorage logic here */}
+              <ImCross style={{ fontSize: '40px', cursor: 'pointer' }} />
+            </aside>
+          ) : (
+            <aside className='icons'>
+              {/* TODO: Implement add to WatchList logic here*/}
+              <CgPlayListAdd style={{ fontSize: '50px', cursor: 'pointer' }} />
+              {/* TODO: Implement add to Seen It list logic here*/}
+              <IoEyeOutline style={{ fontSize: '50px', cursor: 'pointer' }} />
+            </aside>
+          )}
+        </section>
+      ) : (
+        <h1 style={{ margin: '16px 0' }}>Please search for a film.</h1>
+      )}
+    </>
+  );
+};
+
+export default CandidateCard;
